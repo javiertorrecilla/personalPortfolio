0812e366c854aeb36fc21470c391f04bfd33f61b
Javier Torrecilla Reyes <javiertorrecillareyes@gmail.com>
2025-01-20 22:41:44 +0100

    Creacion Proyecto

---
+++ src/theme/images.js
@@ -0,0 +1,9 @@
+import blueThemeboy from '../assets/svg/about/blueTheme-boy.svg'
+import eduBlue from '../assets/svg/education/eduBlue.svg'
+import expBlue from '../assets/svg/experience/expBlue.svg'
+import contactsBlue from '../assets/svg/contacts/contactsBlue.svg'
+
+
+export { 
+    blueThemeboy,eduBlue,expBlue,contactsBlue
+}
