dfae7d6f8e4cbab4ded11e361bc13ab12aa2375f
Javier Torrecilla Reyes <javiertorrecillareyes@gmail.com>
2025-07-24 20:35:10 +0200

    Update skillsData.js

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,110 +1,45 @@
- export const skillsData = [
-    'HTML',
-    'CSS',
-    'javascript' ,
-    'React',
-    'Docker',
-    'Microsoft Office',
-    'WolframAlpha',
-    'Bootstrap',
-    'C',
-    'C++',
-    'C#',
-    'Python',
-    'Java',
-    'Matlab',
-    'Git',
-    'Canva',
-    'MongoDB',
-    'MySQL',
-    'AWS',
-    'Firebase',
-    'Wordpress',
-]
+import html from '../assets/svg/skills/html.svg';
+import css from '../assets/svg/skills/css.svg';
+import javascript from '../assets/svg/skills/javascript.svg';
+import react from '../assets/svg/skills/react.svg';
+import docker from '../assets/svg/skills/docker.svg';
+import microsoftoffice from '../assets/svg/skills/microsoftoffice.svg';
+import wolframalpha from '../assets/svg/skills/wolframalpha.svg';
+import bootstrap from '../assets/svg/skills/bootstrap.svg';
+import c from '../assets/svg/skills/c.svg';
+import cplusplus from '../assets/svg/skills/cplusplus.svg';
+import csharp from '../assets/svg/skills/csharp.svg';
+import python from '../assets/svg/skills/python.svg';
+import java from '../assets/svg/skills/java.svg';
+import matlab from '../assets/svg/skills/matlab.svg';
+import git from '../assets/svg/skills/git.svg';
+import canva from '../assets/svg/skills/canva.svg';
+import mongoDB from '../assets/svg/skills/mongoDB.svg';
+import mysql from '../assets/svg/skills/mysql.svg';
+import aws from '../assets/svg/skills/aws.svg';
+import firebase from '../assets/svg/skills/firebase.svg';
+import wordpress from '../assets/svg/skills/wordpress.svg';
 
-// Choose your skills from below. Make sure it's in the same format and spelled correctly.
-// Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-
-
-// AVAILABLE SKILLS
-
-/* 
-    HTML
-    CSS
-    JS 
-    React
-    Next JS
-    Nuxt JS
-    Node JS
-    Vue
-    Angular
-    Docker
-    Photoshop
-    Illustrator
-    Svelte
-    GCP
-    Azure
-    Fastify
-    Haxe
-    Ionic
-    Markdown
-    Microsoft Office
-    Picsart
-    Sketch
-    Unity
-    WolframAlpha
-    Adobe XD
-    After Effects
-    Bootstrap
-    Bulma
-    CapacitorJs
-    Coffeescript
-    MemSQL
-    C
-    C++
-    C#
-    Python
-    Java
-    Julia
-    Matlab
-    Swift
-    Ruby
-    Kotlin
-    Go
-    PHP
-    Flutter
-    Dart
-    Typescript
-    Swift
-    Git
-    Figma
-    Canva
-    Ubuntu
-    Bootstrap
-    MongoDB
-    Tailwind
-    ViteJS
-    VuetifyJS
-    MySQL
-    PostgreSQL
-    AWS
-    Firebase
-    Blender
-    Premiere Pro
-    Adobe Audition
-    Deno
-    Django
-    Gimp
-    Graphql
-    Lightroom
-    MaterialUI
-    Nginx
-    Numpy
-    OpenCV
-    Pytorch
-    Selenium
-    Strapi
-    Tensorflow
-    Webex
-    Wordpress
-*/
+export const skillsData = [
+  { name: 'HTML', icon: html },
+  { name: 'CSS', icon: css },
+  { name: 'JavaScript', icon: javascript },
+  { name: 'React', icon: react },
+  { name: 'Docker', icon: docker },
+  { name: 'Microsoft Office', icon: microsoftoffice },
+  { name: 'WolframAlpha', icon: wolframalpha },
+  { name: 'Bootstrap', icon: bootstrap },
+  { name: 'C', icon: c },
+  { name: 'C++', icon: cplusplus },
+  { name: 'C#', icon: csharp },
+  { name: 'Python', icon: python },
+  { name: 'Java', icon: java },
+  { name: 'Matlab', icon: matlab },
+  { name: 'Git', icon: git },
+  { name: 'Canva', icon: canva },
+  { name: 'MongoDB', icon: mongoDB },
+  { name: 'MySQL', icon: mysql },
+  { name: 'AWS', icon: aws },
+  { name: 'Firebase', icon: firebase },
+  { name: 'WordPress', icon: wordpress },
+];
