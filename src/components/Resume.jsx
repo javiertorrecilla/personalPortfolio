b9de31e81345dbeab1c910cf9251f6705074bff1
Javier Torrecilla Reyes <124880910+javiertorrecilla@users.noreply.github.com>
2025-07-26 08:50:24 +0200

    Update Resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,17 @@
 
 
+import Education from "./Education";
+import Experience from "./Experience";
+
 export default function Resume() {
   return (
     <article className="resume active" data-page="resume">
       <header>
         <h2 className="h2 article-title">Currículum</h2>
       </header>
+
+      <Education />
+      <Experience />
 
     </article>
   );
