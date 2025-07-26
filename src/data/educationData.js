48d6e4c300f5d2c1b74547a32a9e7650dbbfedfc
Javier Torrecilla Reyes <124880910+javiertorrecilla@users.noreply.github.com>
2025-07-26 08:47:13 +0200

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,22 @@
 export const educationData = [
     {
-        id: 1,
-        institution: 'Colegio Puertosol',
-        course: 'Educación Secundaria Obligatoria',
-        startYear: '2015',
-        endYear: '2019'
+        institution: 'Universidad de Málaga',
+        course: 'Grado en Ingeniería de Software',
+        startYear: '2021',
+        endYear: '2025'
     },
     {
-        id: 2,
         institution: 'Colegio Puertosol',
         course: 'Bachillerato de Ciencias Tecnológicas',
         startYear: '2019',
         endYear: '2021'
     },
     {
-        id: 3,
-        institution: 'Universidad de Málaga',
-        course: 'Grado en Ingeniería de Software',
-        startYear: '2021',
-        endYear: 'Actualidad'
+        institution: 'Colegio Puertosol',
+        course: 'Educación Secundaria Obligatoria',
+        startYear: '2015',
+        endYear: '2019'
     },
+    
+    
 ]
