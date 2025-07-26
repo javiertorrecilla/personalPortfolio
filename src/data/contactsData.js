bb35b179ed702d931d4900bf2a7296838e4595f7
Javier Torrecilla Reyes <124880910+javiertorrecilla@users.noreply.github.com>
2025-07-26 22:04:48 +0200

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,7 @@
 export const contactsData = {
     email: 'javiertorrecillareyes@gmail.com',
     phone: '+34 622 680 875',
-    address: 'Málaga',
-    sheetAPI: ''
+    address: 'Málaga, Andalucía, España',
+    date: '16/01/2003',
+    name: 'Javier Torrecilla Reyes',
 }
