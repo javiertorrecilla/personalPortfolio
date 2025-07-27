c557b87b355223b329b140489cd84cbba83e2c30
Javier Torrecilla Reyes <124880910+javiertorrecilla@users.noreply.github.com>
2025-07-27 22:38:49 +0200

    Update Blog.jsx

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,13 @@
+import CertificationsSection from './CertificationsSection';
+
 export default function Certificaciones() {
   return (
     <article className="blog active" data-page="blog">
       <header>
         <h2 className="h2 article-title">Certificaciones</h2>
       </header>
+
+      <CertificationsSection />
 
     </article>
   );
