973d462f5a877e460c5700028c87389e24cd715d
Javier Torrecilla Reyes <javiertorrecillareyes@gmail.com>
2025-07-29 10:45:11 +0200

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import Skills from "./Skills";
+import {aboutData} from "../data/aboutData";
 
 export default function About() {
   return (
@@ -8,11 +9,8 @@ export default function About() {
       </header>
 
       <section className="about-text">
-        <p>
-          Soy ingeniero del Software por la Universidad de Málaga, apasionado por la tecnología, el trabajo en equipo y el aprendizaje continuo. Extrovertido y orientado a la colaboración, disfruto trabajando en entornos donde la comunicación y el apoyo mutuo son clave para alcanzar metas comunes. Me encanta enfrentar desafíos como oportunidades para crecer personal y profesionalmente.
-        </p>
-        <p>
-          A lo largo de mi carrera, he adquirido experiencia sólida en desarrollo back-end con Java, Python y Node.js, así como en front-end con React. He participado en varios proyectos web que han fortalecido mis habilidades de resolución de problemas y aplicado mis conocimientos teóricos a escenarios reales. Además, cuento con un nivel B2 de inglés, lo que amplía mis habilidades de comunicación y adaptabilidad cultural.        </p>
+        <p>{aboutData.description1}</p>
+        <p>{aboutData.description2}</p>
       </section>
 
       <Skills />
