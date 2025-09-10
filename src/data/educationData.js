ffce28df77a673cd8b1b8201ef4213e0d91b3c02
Javier Torrecilla Reyes <javiertorrecillareyes@gmail.com>
2025-09-10 11:38:11 +0200

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,6 +1,12 @@
 export const educationData = [
     {
         institution: 'Universidad de Málaga',
+        course: 'Máster en Profesorado de ESO y Bachillerato, Formación Profesional y Enseñanza de Idiomas - Especialidad en Tecnología, Informática y Procesos Industriales',
+        startYear: '2025',
+        endYear: 'Actualidad'
+    },
+    {
+        institution: 'Universidad de Málaga',
         course: 'Grado en Ingeniería de Software',
         startYear: '2021',
         endYear: '2025'
