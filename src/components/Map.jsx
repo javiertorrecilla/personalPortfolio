5ad8e8159661d02656af6e72a33fde1b98ac1c90
Javier Torrecilla Reyes <javiertorrecillareyes@gmail.com>
2025-09-10 12:24:18 +0200

    Update Map.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,7 +5,7 @@ export default function Map() {
     <section className="mapbox" data-mapbox>
       <figure>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3183.625905511212!2d-4.428055284587129!3d36.721261679964794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f79cd7e05fbf%3A0x4bb92df7580129e6!2sM%C3%A1laga!5e0!3m2!1ses!2ses!4v1699983212345!5m2!1ses!2ses"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51170.89540740528!2d-4.49046634305642!3d36.71821490105091!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd7259c44fdb212d%3A0x6025dc92c9ca32cf!2zTcOhbGFnYQ!5e0!3m2!1ses!2ses!4v1757499487695!5m2!1ses!2ses"
           width="100%"
           height="300"
           loading="lazy"
@@ -18,3 +18,5 @@ export default function Map() {
     </section>
   );
 }
+
+
